3526dd3575687940c9ab4b14a70d8986c646fdbd
Jörg <joerg.mattes@gmx.net>
2012-03-08 23:08:20 +0700

    x

--- jsf/src/com/joerg/service/MailService.java
+++ jsf/src/com/joerg/service/MailService.java
@@ -22,7 +22,7 @@ public class MailService {
 		Message msg = new MimeMessage(session);
 		msg.setFrom(faustine);
 		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmail, recipientFullname));
-		if (false) { //sendCC) {
+		if (sendCC) {
 			msg.addRecipient(Message.RecipientType.CC, faustine);
 		}
 
